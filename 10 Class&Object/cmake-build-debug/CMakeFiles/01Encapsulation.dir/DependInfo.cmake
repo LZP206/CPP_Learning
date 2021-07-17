1837248eb2eaed6fd8f078635ea66edec7534c3f
LZP206 <86526157+LZP206@users.noreply.github.com>
2021-07-17 21:07:00 +0800

    Add files via upload

---
+++ 10 Class&Object/cmake-build-debug/CMakeFiles/01Encapsulation.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/01Encapsulation.cpp" "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/cmake-build-debug/CMakeFiles/01Encapsulation.dir/01Encapsulation.cpp.o"
+  "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/src/CircleType.cpp" "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/cmake-build-debug/CMakeFiles/01Encapsulation.dir/src/CircleType.cpp.o"
+  "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/src/PointType.cpp" "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/cmake-build-debug/CMakeFiles/01Encapsulation.dir/src/PointType.cpp.o"
+  "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/src/ifInCircle.cpp" "/Users/lizeping/OneDrive/CPP Learning/10 Class&Object/cmake-build-debug/CMakeFiles/01Encapsulation.dir/src/ifInCircle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 10 Class&Object/cmake-build-debug/CMakeFiles/01Encapsulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "01Encapsulation"
+  "01Encapsulation.pdb"
+  "CMakeFiles/01Encapsulation.dir/01Encapsulation.cpp.o"
+  "CMakeFiles/01Encapsulation.dir/src/CircleType.cpp.o"
+  "CMakeFiles/01Encapsulation.dir/src/PointType.cpp.o"
+  "CMakeFiles/01Encapsulation.dir/src/ifInCircle.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01Encapsulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
