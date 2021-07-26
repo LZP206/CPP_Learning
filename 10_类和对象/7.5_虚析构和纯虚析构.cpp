4b8daee2e3579ad4ea549b19eb6efc2c5faff54e
LZP206 <lizeping188@outlook.com>
2021-07-26 13:15:04 +0800

    210726-02

--- 10_类和对象/7.5_虚析构和纯虚析构.cpp
+++ 10_类和对象/7.5_虚析构和纯虚析构.cpp
@@ -14,7 +14,7 @@ class Animal
     Animal(){cout << "Animal构造函数调用" << endl;}
 
     // 虚析构可以解决父类指针释放子类对象不干净的问题
-    virtual ~Animal() {cout << "Animal虚析构函数调用" << endl;}
+    ~Animal() {cout << "Animal虚析构函数调用" << endl;}
 
     // virtual ~Animal() = 0; // 函数声明：纯虚析构  
 
@@ -84,15 +84,15 @@ class Cat : public Animal
 
 
 
-//子类创建在堆区new:需要delete
-void test01()
-{
-    Animal * animal  = new Cat("Tom");
-    animal->speak();
-    cout << "test01函数调用" << endl;
-    delete animal;
+//子类创建在堆区new:需要手动delete
+// void test01()
+// {
+//     Animal * animal  = new Cat("Tom");
+//     animal->speak();
+//     cout << "test01函数调用" << endl;
+//     delete animal;
 
-}
+// }
 
 
 //子类没有创建在堆区:不用delete，基类和派生类的构造和析构函数都会被都调用
@@ -111,7 +111,7 @@ void test02()
 
 int main()
 {
-    test01();
+    test02();
     cout << "main函数调用" << endl;
     return 0;
 }
