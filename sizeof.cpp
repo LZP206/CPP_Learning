a1bb933699184a79e6e9e25d102159204531dbe8
LZP206 <lizeping188@outlook.com>
2022-08-30 10:49:19 +0800

    内存对齐

--- sizeof.cpp
+++ sizeof.cpp
@@ -1,14 +1,73 @@
 using namespace std;
 #include <iostream>
 
+struct AAA{
+    int a1;
+    int a2;
+    double d1;
+};
+struct  BBB{
+    int a1;
+    double d1;
+    int a2;
+};
+struct CCC{
+    short a;
+    int b;
+    char c;
+};
+
+struct DDD{
+    short a;
+    char c;
+    int b;
+};
+
+
+
+class AA {
+    public:
+    int a;
+    AA(){}
+    virtual ~AA(){}
+};
+class BB {
+    public:
+    int a;
+    double d;
+    BB(){}
+    virtual ~BB(){}
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 int main() {
-    int (*A)[5];// æ•°ç»„æŒ‡é’ˆï¼Œè¡¨ç¤ºæŒ‡å‘5ä¸ªintæ•°ç»„çš„æŒ‡é’ˆ
+    int (*A)[5];// Êı×éÖ¸Õë£¬±íÊ¾Ö¸Ïò5¸öintÊı×éµÄÖ¸Õë
     cout << sizeof(A) << endl;
     cout << sizeof(*(A+1)) << endl;
-
     int a[5] = {1,2,3,4,5};
-    cout << *(a+1) << "ä¸" << a[1] << "ç­‰ä»·" <<endl;
+    cout << *(a+1) << "Óë" << a[1] << "µÈ¼Û" <<endl;
+
+    AAA aaa; cout << sizeof(aaa) << endl;  // 16 = 4+4+8 ÄÚ´æ¶ÔÆë£¬µÚÒ»¸ö8×Ö½ÚÀï¿ÉÒÔ·ÅÁ½¸öint
+    BBB bbb; cout << sizeof(bbb) << endl;  // 24 = 8+8+8
+    CCC ccc; cout << sizeof(ccc) << endl;  // 12 = 4+4+4 
+    DDD ddd; cout << sizeof(ddd) << endl;  // 8 = 2+2+4
+
+    AA aa; cout << sizeof(aa) << endl;  // 16
+    BB bb; cout << sizeof(bb) << endl;  // 24
+
+
+    system("pause");
     return 0;
 }
